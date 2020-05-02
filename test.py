a70790958dd7d8c136ddcc369c07854552dc205d
zenemaxx <64691335+zenemaxx@users.noreply.github.com>
2020-05-02 15:50:48 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,38 @@
+import discord
+import os
+from discord.ext import commands
+from datetime import datetime
+today = datetime.now().date()
+tm = datetime.now()
+vrem = "   {}:{}".format(tm.hour, tm.minute)
+today = datetime.now().date()
+tm = datetime.now()
+vrem = "   {}:{}".format(tm.hour, tm.minute)
+Bot = commands.Bot(command_prefix = '!')
+@Bot.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(Bot))
+
+@Bot.command( pass_context = True)
+@commands.has_any_role(678646632321122345,665171381339815946)
+async def  kick(ctx, member: discord.Member, *, reason = None):
+    await ctx.channel.purge(limit = 1)#СТИРАЕМ СООБЩЕНИЕ С КОММАНДОЙ .kcik
+
+    await member.kick(reason = reason)
+    await ctx.send(f"юзер {member.mention} кикнут за плохое поведение")
+
+@Bot.command( pass_context = True)
+@commands.has_any_role(678646632321122345,665171381339815946)
+async def banan(ctx, member: discord.Member, *, reason = None):
+    emb = discord.Embed(title = '{}  в {}'.format(today, vrem), color = discord.Color.red())
+    await ctx.channel.purge(limit = 1)#удаляем сообщение с этой командой из чата
+
+    emb.set_author(name = member.name, icon_url = member.avatar_url)#Показываем имя и аватар забаненого пользователя
+    emb.add_field(name = 'Ban user', value = 'Юзер {}'.format(member) + ' забанен по причине "{}" '.format(reason))
+    emb.set_footer(text ="Был забанен администратором {} 'ом".format(ctx.author.name) , icon_url = ctx.author.avatar_url)
+
+    await ctx.send(embed = emb)
+    await member.ban(reason = reason)
+  
+token = os.environ.get('BOT_TOKEN')
+Bot.run(str(token))
