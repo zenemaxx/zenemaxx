2dd087dac5b3ce463b9a63fa60179fbd98eca51d
zenemaxx <64691335+zenemaxx@users.noreply.github.com>
2020-05-05 01:01:39 +0300

    Update test.py

--- test.py
+++ test.py
@@ -180,5 +180,14 @@ async def role(ctx, role: str ):
 	else:
 		await ctx.send(f'У вас слишком много ролей!')
 
+#removerole
+@Bot.command()
+@commands.has_any_role("kicker" )
+async def removerole(ctx, member: discord.Member, role: str ):
+	await ctx.channel.purge(limit = 1)
+	role = discord.utils.get( ctx.message.guild.roles, name = role)
+	await member.remove_roles( role )
+	await ctx.send(f' { member } получил роль {role}')
+
 token = os.environ.get('BOT_TOKEN')
 Bot.run(str(token))
